f5d091ff583672357902491e25a0b5b5f8363b64
StevenRP97 <lstevenrpl97@gmail.com>
2024-09-04 19:53:13 -0600

    Adding the lsp plugin but not working yet

--- lua/options.lua
+++ lua/options.lua
@@ -10,5 +10,4 @@ vim.g.mapleader = " "
 vim.keymap.set('n','<C-f>', ':source %<CR>', {})
 vim.keymap.set('n','<C-s>', ':wa<CR>', {})
 vim.keymap.set('n','<A-q>', ':q<CR>', {})
-vim.keymap.set('n', '<A-g>', 'git add .', {})
 
