baf574f09ec35202a0dd5e57df1e43bf6b0d5010
StevenRP97 <lstevenrpl97@gmail.com>
2024-09-07 10:49:31 -0600

    This is the latest modification in order to test how the plugin goes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,8 +1,11 @@
 -- Keyboard shortcuts 
-vim.keymap.set('n','<C-f>', ':source %<CR>', {})
 vim.keymap.set('n','<C-s>', ':wa<CR>', {})
 vim.keymap.set('n','<A-q>', ':q<CR>', {})
 vim.keymap.set('n','<A-S-q>', ':q!<CR>', {})
 vim.keymap.set('n','<A-a>', ':wincmd k<CR>', {})
 vim.keymap.set('n','<A-z>', ':wincmd j<CR>', {})
+
+vim.keymap.set('n','<C-A-a>', ':Git add .<CR>', {})
+vim.keymap.set('n','<C-A-c>', ':Git commit -m ""', {})
+vim.keymap.set('n','C-A-p>', ':Git push<CR>', {})
 
