6f697490af8bb39d1b6d9d2822e6d1954db4b21f
StevenRP97 <lstevenrpl97@gmail.com>
2024-09-17 12:14:54 -0600

    Adding Emmet and modifying a couple of things inside

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,7 @@ vim.keymap.set('n','<A-S-q>', ':q!<CR>', {})
 vim.keymap.set('n','<A-a>', ':wincmd k<CR>', {})
 vim.keymap.set('n','<A-z>', ':wincmd j<CR>', {})
 
+-- Git related keybinds
 vim.keymap.set('n','<C-A-a>', ':Git add .<CR>', {})
 vim.keymap.set('n','<C-A-c>', ':Git commit -m ""', {})
 vim.keymap.set('n','<C-A-p>', ':Git push<CR>', {})
--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -1,6 +1,3 @@
 return{
   'mattn/emmet-vim'
---  config = function()
---    vim.keymap.set('let g:user_emmet_leader_key='<C-y>'end')
---  end
 }
