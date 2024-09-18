f396d78e739e0e12b49d5f6d97fa9f88a698a644
StevenRP97 <lstevenrpl97@gmail.com>
2024-09-17 19:45:52 -0600

    Tiny mod

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,7 +4,7 @@ vim.keymap.set('n','<A-q>', ':q<CR>', {})
 vim.keymap.set('n','<A-S-q>', ':q!<CR>', {})
 vim.keymap.set('n','<A-a>', ':wincmd k<CR>', {})
 vim.keymap.set('n','<A-z>', ':wincmd j<CR>', {})
-vim.keymap.set('n','<C-d>', 'diw<CR>', {})
+vim.keymap.set('n','<C-d>', 'diw', {})
 
 -- Git related keybinds
 vim.keymap.set('n','<C-A-a>', ':Git add .<CR>', {})
