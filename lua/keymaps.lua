d748da24419568cf03fc22ddcf7078f8beb1bff1
StevenRP97 <lstevenrpl97@gmail.com>
2024-09-17 19:31:14 -0600

    Adding keymap to quick execute diw

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,7 @@ vim.keymap.set('n','<A-q>', ':q<CR>', {})
 vim.keymap.set('n','<A-S-q>', ':q!<CR>', {})
 vim.keymap.set('n','<A-a>', ':wincmd k<CR>', {})
 vim.keymap.set('n','<A-z>', ':wincmd j<CR>', {})
+vim.keymap.set('n','<C-d>', 'diw<CR>', {})
 
 -- Git related keybinds
 vim.keymap.set('n','<C-A-a>', ':Git add .<CR>', {})
