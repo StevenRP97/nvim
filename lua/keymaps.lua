c719aafca24b5038df9a23f0f1fa4c883b369d57
StevenRP97 <lstevenrpl97@gmail.com>
2024-09-24 19:08:56 -0600

    Adding key navigation to keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,12 +2,20 @@
 vim.keymap.set('n','<C-s>', ':wa<CR>', {})
 vim.keymap.set('n','<A-q>', ':q<CR>', {})
 vim.keymap.set('n','<A-S-q>', ':q!<CR>', {})
-vim.keymap.set('n','<A-a>', ':wincmd k<CR>', {})
-vim.keymap.set('n','<A-z>', ':wincmd j<CR>', {})
 vim.keymap.set('n','<C-d>', 'diw', {})
 
 -- Git related keybinds
 vim.keymap.set('n','<C-A-a>', ':Git add .<CR>', {})
 vim.keymap.set('n','<C-A-c>', ':Git commit -m ""', {})
 vim.keymap.set('n','<C-A-p>', ':Git push<CR>', {})
+
+-- Keymaps to navigate across the windows
+vim.keymap.set('n','<A-k>', ':wincmd k<CR>', {})
+vim.keymap.set('n','<A-j>', ':wincmd j<CR>', {})
+vim.keymap.set('n','<A-h>', ':wincmd h<CR>', {})
+vim.keymap.set('n','<A-l>', ':wincmd l<CR>', {})
+
+-- Arduino related 
+--vim.keymap.set('n', '<leader>ca', ':!arduino-cli compile --fqbn arduino:avr:uno %<CR>', {})
+--vim.keymap.set('n', '<leader>up', ':!arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:uno %<CR>', {})
 
