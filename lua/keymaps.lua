e062c90632922161a3ea8570c0ebb02bfadbb070
StevenRP97 <lstevenrpl97@gmail.com>
2024-10-11 18:56:46 -0600

    Adding the startup plugin and some other extra changes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,4 @@
--- Keyboard shortcuts 
+-- Keymaps console quick interactions
 vim.keymap.set('n','<C-s>', ':wa<CR>', {})
 vim.keymap.set('n','<A-q>', ':q<CR>', {})
 vim.keymap.set('n','<A-S-q>', ':q!<CR>', {})
@@ -10,8 +10,11 @@ vim.keymap.set('n','<C-A-c>', ':Git commit -m ""', {})
 vim.keymap.set('n','<C-A-p>', ':Git push<CR>', {})
 
 -- Keymaps to navigate across the windows
-vim.keymap.set('n','<A-k>', ':wincmd k<CR>', {})
-vim.keymap.set('n','<A-j>', ':wincmd j<CR>', {})
-vim.keymap.set('n','<A-h>', ':wincmd h<CR>', {})
-vim.keymap.set('n','<A-l>', ':wincmd l<CR>', {})
+vim.keymap.set('n','<A-Up>', ':wincmd k<CR>', {})
+vim.keymap.set('n','<A-Down>', ':wincmd j<CR>', {})
+vim.keymap.set('n','<A-Left>', ':wincmd h<CR>', {})
+vim.keymap.set('n','<A-Right>', ':wincmd l<CR>', {})
+
+-- "Useless" commands
+vim.keymap.set('n','<A-a>', ':Startup display<CR>', {})
 
